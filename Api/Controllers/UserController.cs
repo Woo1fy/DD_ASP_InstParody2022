b32181d719e45ffc7bb332d0e65408c6d8723dcf
CrunchyPike <crunchypike@gmail.com>
2022-10-28 20:39:55 +0400

    2-nd Lesson (without comment)

---
+++ Api/Controllers/UserController.cs
@@ -0,0 +1,46 @@
+﻿using Api.Models;
+using Api.Services;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using DAL;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserService _userService;
+
+        public UserController(UserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost]
+        public async Task CreateUser(CreateUserModel model) => await _userService.CreateUser(model);
+
+        [HttpGet]
+        [Authorize]
+        public async Task<List<UserModel>> GetUsers() => await _userService.GetUsers();
+
+        [HttpGet]
+        [Authorize]
+        public async Task<UserModel> GetCurrentUser()
+        {
+            var userIdString = User.Claims.FirstOrDefault(x => x.Type == "id")?.Value;
+            if (Guid.TryParse(userIdString, out var userId))
+            {
+
+                return await _userService.GetUser(userId);
+            }
+            else
+                throw new Exception("you are not authorized");
+
+        }
+    }
+}
